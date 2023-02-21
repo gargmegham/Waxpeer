f1035711d1b19e7f8d7660ccd1fec9afea841587
Megham Garg <gargmegham3@gmail.com>
2023-02-21 18:56:55 +0530

    settings page

--- src/pages/settings.tsx
+++ src/pages/settings.tsx
@@ -1,12 +1,28 @@
+import prisma from "../lib/prisma";
 import Layout from "../components/Layout";
+import { GetServerSideProps } from "next";
+import { Card, Row } from "antd";
 import React from "react";
 
-const Settings: React.FC = () => {
+const Settings: React.FC<any> = ({ settings }) => {
   return (
     <Layout>
-      <div></div>
+      <Card title="Settings">
+        <Row></Row>
+        <Row></Row>
+        <Row></Row>
+      </Card>
     </Layout>
   );
+};
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const settings: any = await prisma.settings.findMany();
+  return {
+    props: {
+      settings,
+    },
+  };
 };
 
 export default Settings;
