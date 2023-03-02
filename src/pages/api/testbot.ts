67c9bd6e82de0f69e4bf9c7861066aecfe966844
Hariprasad K B <hariwin7@gmail.com>
2023-03-02 20:05:04 +0530

    merge

--- src/pages/api/testbot.ts
+++ src/pages/api/testbot.ts
@@ -2,6 +2,7 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import { signingKey } from "../../constants";
 import { waxPeerBot } from "../../bot/waxpeer";
 import { priceEmpireBot } from "@/bot/priceempire";
+import { updateFloatBot } from "@/bot/updatefloat";
 
 // GET /api/testbot
 export default async function handle(
@@ -10,6 +11,7 @@ export default async function handle(
 ) {
   try {
     if (req.method === "GET") {
+<<<<<<< Updated upstream
       const jwt = require("jsonwebtoken");
       const token = req.headers.authorization?.split(" ")[1];
       if (!token) {
@@ -19,6 +21,23 @@ export default async function handle(
       if (!decoded) {
         return res.status(401).json({ error: "Unauthorized" });
       }
+=======
+      // verify bearer token
+      // const jwt = require("jsonwebtoken");
+      // const token = req.headers.authorization?.split(" ")[1];
+      // if (!token) {
+      //   return res.status(401).json({ error: "Unauthorized" });
+      // }
+      // const decoded = jwt.verify(token, signingKey);
+      // if (!decoded) {
+      //   return res.status(401).json({ error: "Unauthorized" });
+      // }
+      //   update settings
+
+      //   cronSchedule(priceEmpireBot);
+      //   priceEmpireBot();
+      //   updateFloatBot();
+>>>>>>> Stashed changes
       waxPeerBot();
       priceEmpireBot();
       return res.status(200).json({ status: true });
