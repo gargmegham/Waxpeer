5d98199c54e7f39a2e8bb98837ce41f365d0e1d4
Hariprasad K B <hariwin7@gmail.com>
2023-03-02 20:07:02 +0530

    merge issue fix

--- src/pages/api/testbot.ts
+++ src/pages/api/testbot.ts
@@ -11,7 +11,6 @@ export default async function handle(
 ) {
   try {
     if (req.method === "GET") {
-<<<<<<< Updated upstream
       const jwt = require("jsonwebtoken");
       const token = req.headers.authorization?.split(" ")[1];
       if (!token) {
@@ -21,23 +20,7 @@ export default async function handle(
       if (!decoded) {
         return res.status(401).json({ error: "Unauthorized" });
       }
-=======
-      // verify bearer token
-      // const jwt = require("jsonwebtoken");
-      // const token = req.headers.authorization?.split(" ")[1];
-      // if (!token) {
-      //   return res.status(401).json({ error: "Unauthorized" });
-      // }
-      // const decoded = jwt.verify(token, signingKey);
-      // if (!decoded) {
-      //   return res.status(401).json({ error: "Unauthorized" });
-      // }
-      //   update settings
-
-      //   cronSchedule(priceEmpireBot);
-      //   priceEmpireBot();
-      //   updateFloatBot();
->>>>>>> Stashed changes
+      updateFloatBot();
       waxPeerBot();
       priceEmpireBot();
       return res.status(200).json({ status: true });
