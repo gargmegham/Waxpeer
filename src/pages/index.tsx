6455cc4acac580b6d1b750d0a5d9b0a6bfd92ed5
Megham Garg <gargmegham3@gmail.com>
2023-03-09 21:12:55 +0530

    loading flag

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,17 @@
 import React, { useState } from "react";
 import Router from "next/router";
+import { Spin } from "antd";
 import styles from "@/styles/Login.module.css";
 
 const Login: React.FC = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
 
   const submitData = async (e: React.SyntheticEvent) => {
     e.preventDefault();
     try {
+      setLoading(true);
       const body = { username, password };
       const res = await fetch(`/api/login`, {
         method: "POST",
@@ -22,9 +25,11 @@ const Login: React.FC = () => {
         return;
       }
       localStorage.setItem("token", data.token);
+      setLoading(false);
       await Router.push("/listings");
     } catch (error) {
       console.error(error);
+      setLoading(false);
     }
   };
 
@@ -47,12 +52,16 @@ const Login: React.FC = () => {
           type="password"
           value={password}
         />
-        <input
-          className={styles.cta}
-          disabled={!username || !password}
-          type="submit"
-          value="Login"
-        />
+        {loading ? (
+          <Spin />
+        ) : (
+          <input
+            className={styles.cta}
+            disabled={!username || !password}
+            type="submit"
+            value="Login"
+          />
+        )}
       </form>
     </div>
   );
